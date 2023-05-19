ee6e33f21a2d2aa19d4e38f4664f29b764d831b4
Wisnu Wicaksono <63142229+wiscaksono@users.noreply.github.com>
2023-05-20 05:54:57 +0700

    feat: init tailwind

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Text className="text-red-500">
+        Open up App.tsx to start working on your app!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
